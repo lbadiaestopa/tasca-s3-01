b0389db9b81d2065e1ab3c3f3c9434c9833a0488
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-25 11:00:07 +0100

    feat:  atribute changed from array to OlympicGames type, removed addOlympicGames() method and simplified printOlympicsData() method

--- nivell-1/exercici-1/OlympicGamesPrinter.php
+++ nivell-1/exercici-1/OlympicGamesPrinter.php
@@ -4,37 +4,32 @@ require_once __DIR__ . '/OlympicGames.php';
 
 class OlympicGamesPrinter
 {
-    private array $olympicGames = [];
+    private OlympicGames $olympicGames;
 
     public function __construct(OlympicGames $olympicGames)
     {
-        $this->olympicGames[] = $olympicGames;
+        $this->olympicGames = $olympicGames;
     }
 
-    public function addOlympicGames(OlympicGames $olympicGames): void
+    public function printOlympicsData(): void
     {
-        $this->olympicGames[] = $olympicGames;
-    }
-
-    public function printOlympicsData(int $year)
-    {
-        foreach ($this->olympicGames as $og) {
-            if ($og->getYear() === $year) {
-                echo "Olympic Games $year Results:\n";
-            }
-            foreach ($og->getEventsData() as $event) {
-                echo "Event: " . $event->getName() . " on " . $event->getDate() . "\n";
-                foreach ($og->getResultsData() as $result) {
-                    if ($result->getEvent() === $event->getName()) {
-                        foreach ($og->getAthletesData() as $athlete) {
-                            if ($result->getAthlete() === $athlete->getName()) {
-                                echo "- " . $result->getAthlete() . " from " . $athlete->getCountry() . " won " . $result->getMedal() . "\n";
-                            }
-                        }
-                    }
+        echo $this->olympicGames;
+        foreach ($this->olympicGames->getEventsData() as $event) {
+            foreach ($this->olympicGames->getResultsData() as $result) {
+                if ($event->getName() === $result->getEvent()) {
+                    echo $event . PHP_EOL . $this->matchResultsWithAthletes($result->getAthlete()) . PHP_EOL;
                 }
             }
-                
+        }
+    }
+
+    public function matchResultsWithAthletes(string $athlete): ?string
+    {
+        foreach ($this->olympicGames->getResultsData() as $result) {
+            foreach ($this->olympicGames->getAthletesData() as $a)
+                if ($athlete === $a->getName()) {
+                    return $a . ' won ' . $result->getMedal();
+                }
         }
     }
 }
