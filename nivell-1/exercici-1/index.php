9f9fabc836c2b5c9d4e5155bbe339d7091f856b4
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-21 12:43:04 +0200

    refactor

--- nivell-1/exercici-1/index.php
+++ nivell-1/exercici-1/index.php
@@ -7,19 +7,29 @@ require_once __DIR__ . '/Event.php';
 require_once __DIR__ . '/Result.php';
 
 $athletesData = [
-    ['name' => 'Usain Bolt', 'country' => 'Jamaica'],
-    ['name' => 'Michael Phelps', 'country' => 'USA']
+    'Usain Bolt' => new Athlete('Usain Bolt', 'Jamaica'),
+    'Michael Phelps' => new Athlete('Michael Phelps', 'USA')
 ];
 
 $eventsData = [
-    ['event' => '100m Sprint', 'date' => '2024-08-01'],
-    ['event' => 'Swimming', 'date' => '2024-08-02']
+    '100m Sprint' => new Event('100m Sprint', '2024-08-01'),
+    'Swimming' => new Event('Swimming', '2024-08-02')
 ];
 
-$resultsData = [
+$results = [
     ['athlete' => 'Usain Bolt', 'event' => '100m Sprint', 'medal' => 'Gold'],
     ['athlete' => 'Michael Phelps', 'event' => 'Swimming', 'medal' => 'Gold']
 ];
+
+$resultsData = [];
+
+foreach ($results as $result) {
+    $resultsData[] = new Result(
+        $athletesData[$result['athlete']],
+        $eventsData[$result['event']],
+        $result['medal']
+    );
+}
 
 $olympics = new OlympicGames(2012, $athletesData, $eventsData, $resultsData);
 $olympicGamesPrinter = new OlympicGamesPrinter($olympics);
